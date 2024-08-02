ce1602c5c82671629db84943f18c8cea80721a36
Kamil897 <kamil2007shakirov@gmail.com>
2024-08-02 11:19:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// function hello(a, b, c) {
+//     console.log(a  + b + c);
+// }
+
+// hello(10, 20, 30)
+
+// const fn = function(a, b) {
+//     console.log(a + b);
+// } 
+
+// fn(10, 20)
+// fn(9, 1)
+// fn(30, 70)
+
+
+// const plus = (a, b) => console.log(a + b);
+
+// console.log(plus(16, 17));
+
+// function hello(a, b, c) {
+//     console.log(a + b + c);
+// }
+
+// hello(20, 10, 15);
+
+// const fn = function(a, b) {
+//     console.log(a * b);
+// }
+
+// fn(10, 5)
+
+// const minus = (a, b) => {
+//     console.log(a - b);
+// }
+
+// minus(30, 16)
+
+// const delenie = (a, b) => {
+//     console.log(a / b);
+// }
+
+// delenie(10, 2)
+
+
+// const me = (a, b, c) => {
+//     console.log('' + 0.a, b, c);
+// }
+// me(`abbos`, `valiev`, 21)
+
+
+// const fn2 = (a) => {
+//     if (a == 9) {
+//     }else if(a == 0) {
+// console.log(`это 0`);
+//     } else if ( a == 2) {
+// console.log(`это отрицательное число`);
+//     }
+// }
+
+// fn2(prompt(`Введи число`))
+
+const person = (name) => {
+    console.log(`привет ${name} ${surname}, мне ${age} лет`);
+}
+
+person(`Abbos`, `Валиев`, 21)
